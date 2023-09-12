13d9024caac917f38d943f650b8b22d8644cd68a
Vv-kK <veronicakylie1@gmail.com>
2023-09-12 22:26:53 +0700

    Mengerjakan soal bonus

--- main/models.py
+++ main/models.py
@@ -5,5 +5,5 @@ class Item(models.Model):
     name = models.CharField(max_length=255) #album name
     amount = models.IntegerField() #amount of versions
     artist = models.CharField(max_length=50) 
-    date_release = models.DateField(auto_now_add=True)
+    release_date = models.DateField(auto_now_add=True)
     description = models.TextField()
--- main/tests.py
+++ main/tests.py
@@ -2,6 +2,8 @@ from django.test import TestCase, Client
 
 # Create your tests here.
 from django.test import TestCase, Client
+from main.models import Item
+import datetime
 
 class mainTest(TestCase):
     def test_main_url_is_exist(self):
@@ -11,3 +13,18 @@ class mainTest(TestCase):
     def test_main_using_main_template(self):
         response = Client().get('/main/')
         self.assertTemplateUsed(response, 'main.html')
+    
+                
+    def test_created_items(self):
+        Item.objects.create(name="Attaca", amount=3, artist="Seventeen",
+                            release_date=datetime.date(2021, 10, 21), 
+                            description = "9th mini album from Seventeen with the title trach ""Rock With You""")
+        
+        Item.objects.create(name="The War", amount=3, artist="EXO",
+                            release_date=datetime.date(2017, 7, 18),
+                            description="4th full album from EXO with the title track ""Ko Ko Bop""")
+
+        attaca = Item.objects.get(name="Attaca")
+        the_war_exo = Item.objects.get(name="The War")
+        self.assertEqual(attaca.name, "Attaca")
+        self.assertEqual(the_war_exo.name, "The War")
