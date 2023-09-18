981525ba66d287f100af981a45b64e7f303b6230
Vv-kK <veronicakylie1@gmail.com>
2023-09-18 21:35:31 +0700

    Membuat input form dan menampilkan objek

--- main/models.py
+++ main/models.py
@@ -5,5 +5,4 @@ class Item(models.Model):
     name = models.CharField(max_length=255) #album name
     amount = models.IntegerField() #amount of versions
     artist = models.CharField(max_length=50) 
-    release_date = models.DateField(auto_now_add=True)
     description = models.TextField()
